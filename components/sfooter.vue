aa131b68e594ae1cf80928e7968171b4995cd86f
Abdullah Khan <khan.abdullahaha@icloud.com>
2025-01-25 02:42:24 +0500

    Update sfooter.vue

--- components/sfooter.vue
+++ components/sfooter.vue
@@ -11,7 +11,7 @@ const links = [
 <template>
   <footer class="bg-gray-800">
     <section class="mx-auto max-w-7xl px-2 sm:px-4 lg:py-6 text-gray-300 flex flex-row">
-      <p class="flex-1">&copy; Your Name 2023. All Rights Reserved</p>
+      <p class="flex-1">&copy; Abdullah Khan. All Rights Reserved</p>
       <div class="flex flex-row justify-center gap-4 mb-4 mt-2">
         <a v-for="link in links" :key="link.name" :href="link.href">
           <Icon :name="link.icon" size="1.5rem" class="text-gray-300" />
